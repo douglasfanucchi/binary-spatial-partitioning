322050ed8ea68d4f375ad457af3d31b300c4d85e
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-16 16:20:21 -0300

    tests: test creates vector not located at origin

--- tests/unit/elements/Vector2D.cpp
+++ tests/unit/elements/Vector2D.cpp
@@ -10,7 +10,18 @@ void test_should_create_a_vector_located_at_origin()
     ASSERT_TRUE(v.getTail() == origin);
 }
 
+void test_should_create_a_vector_that_isnt_located_at_origin()
+{
+    Dot p1 = Dot(Fixed(5), Fixed(5)), p2 = Dot(Fixed(1), Fixed(1));
+
+    Vector2D v(p1, p2);
+
+    ASSERT_TRUE(v.getTip() == p1);
+    ASSERT_TRUE(v.getTail() == p2);
+}
+
 void RUN_2D_VECTOR_TEST_SUITE()
 {
     test_should_create_a_vector_located_at_origin();
+    test_should_create_a_vector_that_isnt_located_at_origin();
 }
