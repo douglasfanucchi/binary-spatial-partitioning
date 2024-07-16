8548521e3f9fce56af14ebdab80f138e4f56c708
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-16 15:59:44 -0300

    tests: test compares two dots as equals

--- tests/unit/elements/Dot.cpp
+++ tests/unit/elements/Dot.cpp
@@ -9,7 +9,17 @@ void test_should_create_a_dot()
     ASSERT_TRUE(dot.getOrdinates() == Fixed(1));
 }
 
+void test_should_compare_two_dots_as_equals()
+{
+    Dot p1(Fixed(2.5), Fixed(2.5)), p2(Fixed(2.5), Fixed(2.5));
+
+    bool result = p1 == p2;
+
+    ASSERT_TRUE(result);
+}
+
 void RUN_DOT_TEST_SUITE()
 {
     test_should_create_a_dot();
+    test_should_compare_two_dots_as_equals();
 }
