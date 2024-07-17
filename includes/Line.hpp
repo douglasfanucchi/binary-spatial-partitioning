e6c2b8128109ba61d17e559a14d9bef727dfac83
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-17 10:04:14 -0300

    tests: test creates line that cannot be represented by a function

--- includes/Line.hpp
+++ includes/Line.hpp
@@ -7,10 +7,12 @@
 class Line {
     private:
         Fixed a, b;
+        bool _isFunction;
     public:
         Line(const Dot&, const Dot&);
         Fixed getAngularCoefficient(void) const;
         Fixed getLinearCoefficient(void) const;
+        bool isFunction(void) const;
 };
 
 #endif
--- tests/unit/elements/Line.cpp
+++ tests/unit/elements/Line.cpp
@@ -10,11 +10,23 @@ void test_should_create_line()
     Fixed angularCoefficient = line.getAngularCoefficient();
     Fixed linearCoefficient = line.getLinearCoefficient();
 
+    ASSERT_TRUE(line.isFunction());
     ASSERT_TRUE(angularCoefficient == Fixed(1));
     ASSERT_TRUE(linearCoefficient == Fixed(0));
+}
+
+void test_should_create_line_parallel_to_y_axis()
+{
+    Dot p1(Fixed(1), Fixed(2));
+    Dot p2(Fixed(1), Fixed(4));
+
+    Line line(p1, p2);
+
+    ASSERT_FALSE(line.isFunction());
 }
 
 void RUN_LINE_TEST_SUITE()
 {
     test_should_create_line();
+    test_should_create_line_parallel_to_y_axis();
 }
