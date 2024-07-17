a0d9190f36cd18459389168f8775ab5f5e261a06
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-17 10:04:36 -0300

    feat: implements Line::isFunction(void)

--- src/elements/Line.cpp
+++ src/elements/Line.cpp
@@ -4,8 +4,13 @@ Line::Line(const Dot &p1, const Dot &p2) {
     Fixed dy(p1.getOrdinates() - p2.getOrdinates());
     Fixed dx(p1.getAbscissa() - p2.getAbscissa());
 
+    if (dx == 0) {
+        this->_isFunction = false;
+        return;
+    }
     this->a = dy/dx;
     this->b = p1.getOrdinates() - this->a * p1.getAbscissa();
+    this->_isFunction = true;
 }
 
 Fixed Line::getAngularCoefficient(void) const {
@@ -14,4 +19,8 @@ Fixed Line::getAngularCoefficient(void) const {
 
 Fixed Line::getLinearCoefficient(void) const {
     return this->b;
+}
+
+bool Line::isFunction(void) const {
+    return this->_isFunction;
 }
